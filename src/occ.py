020cfe669d24e3fd4d899221ef62b76ddcf737e5
Farrel Ahmad <13520110@std.stei.itb.ac.id>
2022-12-05 18:38:24 +0700

    ppp ingfo ingfo occ bisa blm belajar AI :(

--- src/occ.py
+++ src/occ.py
@@ -1,52 +1,54 @@
 from typing import List
-
+import time
+from copy import deepcopy
 # class Resource():
 #     def __init__(self, name: str):
 #         self.name = name
 
+SLEEP_TIME = 0.00000000000001
+
 class Action():
-    def __init__(self, type:str, timestamp:int, resource:str):
+    def __init__(self, type:str, no:int, resource:str):
         # type = "R", "W", "C"
         self.type = type
-        # no : transaction number (timestamp)
-        self.timestamp = timestamp
+        # no : transaction number (no)
+        self.no = no
         # resource : resource name
         self.resource = resource
 
 class Transaction():
-    def __init__(self, timestamp: int):
-        self.start_time = timestamp
+    def __init__(self, no: int):
+        self.transaction_no = no
+        self.start_time = no
         self.validate_time = 0
         self.finish_time = 0
         self.written_resources = []
         self.read_resources = []
         self.isCommitted = False
+        self.action_log = []
     
     def commit(self):
         self.isCommitted = True
     
-    def read(self, resource: str):
-        self.read_resources.append(resource)
+    def read(self, action: Action):
+        self.read_resources.append(action.resource)
+        self.action_log.append(action)
 
-    def write(self, resource: str):
-        self.written_resources.append(resource)
+    def write(self, action: Action):
+        self.written_resources.append(action.resource)
+        self.action_log.append(action)
 
 class OCC():
     def __init__(self, num_of_transaction : int, action_list: List[Action]):
         self.list_transaction = [Transaction(i) for i in range(1, num_of_transaction + 1)]
         self.action_list = action_list
+        self.log = []
     
-    def validate(self, action):
+    def validate(self, action, current_transaction):
         # check if the transaction is valid
         # if valid, commit
         # else abort
 
-        #find the transaction
-        for transaction in self.list_transaction:
-            if action.timestamp == transaction.start_time:
-                current_transaction = transaction
-
-        current_transaction.validate_time = current_transaction.start_time
         # start validate
         valid = True
         for to_be_checked_transaction in self.list_transaction:
@@ -60,33 +62,58 @@ class OCC():
                         if written_resource in current_transaction.read_resources:
                             valid = False
                             break
-        if not valid:
-            raise Exception("INVALID TRANSACTION")
+
         return valid
 
     def start(self):
         idx = 0
-        for action in self.action_list:
+        while(idx < len(self.action_list)):
+            #find the transaction
+            action = self.action_list[idx]
+            # print("action list len=",len(self.action_list))
+            # print(action.no, action.resource)
+            for transaction in self.list_transaction:
+                if action.no == transaction.transaction_no:
+                    current_transaction = transaction
+            #start time
+            if (current_transaction.start_time == 0):
+                current_transaction.start_time = time.time()
+
             if action.type == "C":
-                #find the transaction
-                for transaction in self.list_transaction:
-                    if action.timestamp == transaction.start_time:
-                        current_transaction = transaction
-                current_transaction.commit()
-                print("Transaction " + str(current_transaction.start_time) + " is committed")
+                current_transaction.validate_time = time.time()
+                if (self.validate(action, current_transaction)):
+                    current_transaction.commit()
+                    current_transaction.finish_time = time.time()
+                    print("Transaction " + str(current_transaction.transaction_no) + " is committed")
+                    time.sleep(SLEEP_TIME)
+                    self.log.append(action)                    
+                else:
+                    #rollback
+                    time.sleep(SLEEP_TIME)
+                    print("Transaction " + str(current_transaction.start_time) + " is not committed, aborting")
+                    current_transaction.start_time = time.time()
+                    temp_action_list = deepcopy(self.action_list[:idx+1])
+                    # print("len ca =",len(current_transaction.action_log))
+                    for action in current_transaction.action_log:
+                        temp_action_list.append(action)
+                    temp_action_list.append(Action("C", current_transaction.transaction_no, ""))
+                    rest_action_list = deepcopy(self.action_list[idx+1:])
+                    # print("len ral =",len(rest_action_list))
+                    for action in rest_action_list:
+                        temp_action_list.append(action)
+                    self.action_list = temp_action_list
+
             elif action.type == "R":
                 #find the transaction
-                for transaction in self.list_transaction:
-                    if action.timestamp == transaction.start_time:
-                        current_transaction = transaction
-
-                current_transaction.read(action.resource)
-                print("Transaction " + str(action.timestamp) + " is read " + action.resource)
+                current_transaction.read(action)
+                print("Transaction " + str(current_transaction.transaction_no) + " is read " + action.resource)
+                time.sleep(SLEEP_TIME)
+                self.log.append(action)
             elif action.type == "W":
-                if (self.validate(action)):
-                    current_transaction.write(action.resource)
-                    print("Transaction " + str(action.timestamp) + " is write " + action.resource)
-
+                current_transaction.write(action)
+                print("Transaction " + str(current_transaction.transaction_no) + " is write " + action.resource)
+                time.sleep(SLEEP_TIME)
+                self.log.append(action)
             idx+=1
         
 if __name__ == "__main__":
@@ -98,7 +125,7 @@ if __name__ == "__main__":
     action_list = []
     transaction_count = 0
     for action_str in arr:
-        print(action_str)
+        # print(action_str)
         type = action_str[0]
         no = int(action_str[1])
         if no > transaction_count:
