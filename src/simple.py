f05dd5886827d2996a0566481bf6fe5145c10dbd
IloveNooodles <mgarebaldhie80@gmail.com>
2022-12-05 14:05:41 +0700

    feat: push tc and simple

---
+++ src/simple.py
@@ -0,0 +1,15 @@
+class Simple():
+
+    def __init__(self):
+        self.queue = []
+        self.completed = []
+    
+    def commit():
+        pass
+  
+    def write():
+        pass
+
+    def read():
+        pass
+  
